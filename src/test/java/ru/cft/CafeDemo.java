10c5764b11e2212fa10325e4a353c043fa8cf70c
vjazinin <i.vjazinin@cft.ru>
2019-05-23 11:41:09 +0300

    init commit

---
+++ src/test/java/ru/cft/CafeDemo.java
@@ -0,0 +1,11 @@
+package ru.cft;
+
+public class CafeDemo {
+
+    public void canBrewEspresso() {
+        Cafe cafe = new Cafe();
+        cafe.restockBeans(7);
+
+        Coffee coffee = cafe.brew(CoffeeType.Espresso);
+    }
+}
